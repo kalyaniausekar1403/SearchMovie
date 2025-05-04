ae6205104c1aebbf704f6d19fbb221f3e01e93dd
Kalyani Ausekar <33807466+KalyaniAusekar@users.noreply.github.com>
2025-05-04 11:48:33 +0530

    Glide dependecy added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,5 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.coil.compose)
+    implementation(libs.glide)
 }
